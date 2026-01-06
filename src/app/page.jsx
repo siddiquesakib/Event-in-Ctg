c9b51a31185e4d0565caf624a0ce209618843bef
siddiquesakib <dmsakib1122@gmail.com>
2026-01-06 21:02:55 +0600

    Create Home page with hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,11 +3,13 @@ import Recent6 from "@/component/Recent6";
 import Banner from "@/component/Banner";
 import ExploreByDate from "@/component/ExploreByDate";
 import PopularFormats from "@/component/PopularFormats";
+import FutureEvent from "@/component/FutureEvent";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <FutureEvent />
       <Recent6 />
       <PopularFormats />
       <ExploreByDate />
